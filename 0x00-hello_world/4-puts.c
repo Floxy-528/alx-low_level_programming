6c409a075c19d04474d71095db0ba8b1c7cb9b4d
Floxy-528 <floxyamed96@yahoo.com>
2023-10-05 17:54:44 -0700

    wrote a script that compiles a C file and creates an executable named cisfun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print "/"Programming is like building a multilingual puzzle
+ * main - prints "\"Programming is like building a multilingual puzzle"
  *
- * Return 0
+ * Return: 0(Succes)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
