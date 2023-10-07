4b25dfda34278e87c0da74e35c2d16476106496b
Floxy-528 <floxyamed96@yahoo.com>
2023-10-07 13:28:46 +0000

    C program randoms to the variable n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,19 +14,16 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes here */
+
 	if (n > 0)
-		/*print positive number if n > 0*/
 	{
 		printf("%d is positive/n", n);
 	}
 	else if (n < 0)
-		/*prints negative number if n < 0*/
 	{
 		printf("%d is negative/n", n);
 	}
 	else
-		/*prints zero*/
 	{
 		printf("%d is zero/n", n);
 	}
