34216c1543120efadba374613489148f2739764e
Floxy-528 <floxyamed96@yahoo.com>
2023-10-27 19:00:12 -0700

    program that multiplies two number

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,32 +1,73 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * main - print that add positive number and print results on conditions
- * @argc: number of command line arguments
- * @argv: array of command line arguments
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * Return: Always 0 (Success)
+ * Return: the int converted from the string
  */
-int main(int argc, char **argv)
+int _atoi(char *s)
 {
-	int i, sum = 0;
+	int i, d, n, len, f, digit;
 
-	for (j = 0; j < argc; j++)
-	i = atoi(argv[j]);
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	if (i > 0)
-		sum = sum + i;
+	while (s[len] != '\0')
+		len++;
 
-	else if (i < 0)
-		printf("Error\n");
-	return (1);
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
 
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
 
-	else
-		printf("0\n");
-
-
+	if (f == 0)
 		return (0);
 
+	return (n);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
 }
